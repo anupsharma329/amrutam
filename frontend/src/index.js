2a6d091f90068f96cb1441e378dc5b3999dd3c3f
anupsharma329 <anupsharma329@gmail.com>
2025-07-20 15:24:14 +0530

    s3

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,3 @@
-// Before (index.js)
-import React from 'react';
-import App from './App';
-
-const root = document.getElementById('root');
-// No usage of React or App
-
-// After: Use the imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
