c73a244da296ccca6eb4f149a861a76ca3f561cb
anupsharma329 <anupsharma329@gmail.com>
2025-07-21 21:06:32 +0530

    latest

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div> Hello World this is anupsharma project </div>
+    <div> Hello, This is my Next.js frontend! </div>
   );
 }
